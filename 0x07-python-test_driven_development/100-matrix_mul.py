1cecdbfff9235dbca177c4b92dd0ece025695631
nyabindenyasha@gmail.com <nyabindenyasha@gmail.com>
2022-09-21 20:56:57 +0200

    corrections

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,55 +1,87 @@
 #!/usr/bin/python3
 """
-Contains the matrix_mul function
+A function that multiplies 2 matrices
 """
 
 
 def matrix_mul(m_a, m_b):
-    """Multiply two matrices(lists of lists of integers/floats)"""
-    if type(m_a) is not list:
+    """
+    A function that multiplies 2 matrices
+    """
+
+    if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
-    l1 = len(m_a)
-    if l1 == 0:
-        raise ValueError("m_a can't be empty")
-    l2 = None
-    for i in m_a:
-        if type(i) is not list:
-            raise TypeError("m_a must be a list of lists")
-        if l2 is None:
-            l2 = len(i)
-            if l2 == 0:
-                raise ValueError("m_a can't be empty")
-        if l2 != len(i):
-            raise TypeError("each row of m_a must should be of the same size")
-        for j in i:
-            if type(j) is not int and type(j) is not float:
-                raise TypeError("m_a should contain only integers or floats")
-    if type(m_b) is not list:
+
+    if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    if len(m_b) == 0:
-        raise ValueError("m_b can't be empty")
-    l3 = None
-    for i in m_b:
-        if type(i) is not list:
+
+    for item in m_a:
+        if not isinstance(item, list):
+            raise TypeError("m_a must be a list of lists")
+
+    for item in m_b:
+        if not isinstance(item, list):
             raise TypeError("m_b must be a list of lists")
-        if l3 is None:
-            l3 = len(i)
-            if l3 == 0:
+
+    if not m_a:
+        raise ValueError("m_a can't be empty")
+    else:
+        for l in m_a:
+            if not l:
+                raise ValueError("m_a can't be empty")
+
+    if not m_b:
+        raise ValueError("m_b can't be empty")
+    else:
+        for l in m_b:
+            if not l:
                 raise ValueError("m_b can't be empty")
-        if l3 != len(i):
-            raise TypeError("each row of m_b must should be of the same size")
-        for j in i:
-            if type(j) is not int and type(j) is not float:
+
+    for l in m_a:
+        for item in l:
+            if not isinstance(item, (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+
+    for l in m_b:
+        for item in l:
+            if not isinstance(item, (int, float)):
                 raise TypeError("m_b should contain only integers or floats")
-    if l2 != len(m_b):
+
+    m_a_size = len(m_a[0])
+    for l in m_a:
+        if len(l) != m_a_size:
+            raise TypeError("each row of m_a must should be of the same size")
+
+    m_b_size = len(m_b[0])
+    for l in m_b:
+        if len(l) != m_b_size:
+            raise TypeError("each row of m_b must should be of the same size")
+
+    # find number of columns/items in list[0] of m_a
+    col_count_a = 0
+    for col in m_a[0]:
+        col_count_a += 1
+    # find number of rows in m_a
+    row_count_a = 0
+    for row in m_a:
+        row_count_a += 1
+    # find number of cols in m_b
+    col_count_b = 0
+    for col in m_b[0]:
+            col_count_b += 1
+    # find number of rows in m_b
+    row_count_b = 0
+    for row in m_b:
+        row_count_b += 1
+    # compare the two
+    if col_count_a != row_count_b:
         raise ValueError("m_a and m_b can't be multiplied")
-    matrix = []
-    for i in range(l1):
-        l = []
-        for j in range(l3):
-            n = 0
-            for k in range(l2):
-                n += m_a[i][k] * m_b[k][j]
-            l.append(n)
-        matrix.append(l)
-    return matrix
+
+    # initialize new matrix
+    C = [[0 for row in range(col_count_b)] for col in range(row_count_a)]
+
+    for i in range(row_count_a):
+        for j in range(col_count_b):
+            for k in range(col_count_a):
+                C[i][j] += m_a[i][k] * m_b[k][j]
+    return(C)
